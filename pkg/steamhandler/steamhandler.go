1072fffcc3b0a51afbfba37f8588689473ee98ae
trirandom <tristan.grunert87@gmail.com>
2019-04-03 01:08:59 +0200

    more debug info

--- pkg/steamhandler/steamhandler.go
+++ pkg/steamhandler/steamhandler.go
@@ -38,14 +38,21 @@ func SteamConnect(c *gin.Context) {
 		switch e := event.(type) {
 		case *steam.ConnectedEvent:
 			client.Auth.LogOn(myLoginInfo)
+			fmt.Printf("\nsuccess  A  %#v\n", steamCo)
 		case *steam.MachineAuthUpdateEvent:
 			ioutil.WriteFile("sentry", e.Hash, 0666)
+			fmt.Printf("\nsuccess  B  %#v\n", steamCo)
 		case *steam.LoggedOnEvent:
 			client.Social.SetPersonaState(steamlang.EPersonaState_Online)
+			fmt.Printf("\nsuccess  C  %#v\n", steamCo)
 		case steam.FatalErrorEvent:
 			log.Print(e)
+			fmt.Printf("\nsuccess  D  %#v\n", steamCo)
 		case error:
 			log.Print(e)
+		default:
+			c.JSON(http.StatusAccepted, gin.H{"status": http.StatusAccepted, "message": "Whatever happend, just happend... [A] guess it's a good start", "resourceId": steamCo.UserName})
 		}
 	}
+	c.JSON(http.StatusAccepted, gin.H{"status": http.StatusAccepted, "message": "Whatever happend, just happend... [B] guess it's a good start", "resourceId": steamCo.UserName})
 }
